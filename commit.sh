e05d441cbc93511b65845cda1a12105d956ef396
David Rajcher <david.rajcher@gmail.com>
2023-05-09 11:19:24 +0200

    removed file

--- commit.sh
+++ commit.sh
@@ -5,24 +5,40 @@ git_user_name=$(git config user.name)
 git_user_email=$(git config user.email)
 
 # Set the start date to January 1, 2022
-current_date="2022-01-01"
+current_date="2023-01-01"
+
+# Create the text file
+filename="file.txt"
+touch "$filename"
 
 # Loop through each day of the year
-while [ "$current_date" != "2023-01-01" ]; do
+while [ "$current_date" != "2023-05-08" ]; do
 
-  # Get the day of the week (0-6, where 0 is Sunday and 6 is Saturday)
-  day_of_week=$(date -jf "%Y-%m-%d" "+%w" "$current_date")
+  # Get the day of the week (1-7, where 1 is Monday and 7 is Sunday)
+  day_of_week=$(date -jf "%Y-%m-%d" "$current_date" +"%u")
 
-  # Check if the day is not Saturday (6) or Sunday (0)
+  # Check if the day is not Saturday (6) or Sunday (7)
   if [ "$day_of_week" != "6" ] && [ "$day_of_week" != "7" ]; then
-    # Get a random number between 1 and 4
-    num_files=$((RANDOM % 4 + 1))
+    # Generate a random number between 1 and 100
+    random_num=$((RANDOM % 100 + 1))
 
-    # Create the specified number of files
-    for ((i = 1; i <= num_files; i++)); do
-      # Create a new file with the current date and part number as the filename
-      filename="${current_date}-part${i}.txt"
-      touch "$filename"
+    # Set num_changes based on the probability distribution
+    if [ "$random_num" -le 30 ]; then
+      num_changes=0
+    elif [ "$random_num" -le 55 ]; then
+      num_changes=1
+    elif [ "$random_num" -le 75 ]; then
+      num_changes=2
+    elif [ "$random_num" -le 90 ]; then
+      num_changes=3
+    else
+      num_changes=4
+    fi
+
+    # Make the specified number of changes
+    for ((i = 1; i <= num_changes; i++)); do
+      # Add a character to the text file
+      echo "a" >> "$filename"
 
       # Add the file to Git
       git add "$filename"
@@ -37,7 +53,7 @@ while [ "$current_date" != "2023-01-01" ]; do
       GIT_AUTHOR_EMAIL="$git_user_email" \
       GIT_COMMITTER_NAME="$git_user_name" \
       GIT_COMMITTER_EMAIL="$git_user_email" \
-      git commit -m "Added file for $current_date part ${i}"
+      git commit -m "Change $i on $current_date"
     done
   fi
 
@@ -45,3 +61,6 @@ while [ "$current_date" != "2023-01-01" ]; do
   current_date=$(date -jf "%Y-%m-%d" -v+1d "$current_date" +"%Y-%m-%d")
 
 done
+git push origin main
+
+

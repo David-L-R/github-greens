fd699e773a79e3ea58043c7dc55a029940d88a2b
David Rajcher <david.rajcher@gmail.com>
2024-07-18 18:50:29 +0200

    removed lang

--- java/2024-05-02.java
+++
@@ -1,2 +0,0 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello, World!"); } }
-class Helper { void help() { System.out.println("Helping..."); } }
--- js/2024-03-15.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello, World!");
-function helper() { console.log("Helping..."); }
